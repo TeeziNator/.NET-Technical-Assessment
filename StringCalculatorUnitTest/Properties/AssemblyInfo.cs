10cd122cb0433b9053e12b10b1531382515de727
Themba Khumalo <thembakhumalo11@gmail.com>
2023-03-22 12:57:15 +0200

    Unit tests project for the String Calculator Assessment

---
+++ StringCalculatorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculatorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculatorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f419c1a-3382-43ae-a486-5c238306c2f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCalculatorUnitTest/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String_Calculator;
+using System;
+
+namespace StringCalculatorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator calculate = new Calculator();
+
+        [TestMethod]
+        public void TestAddForEmptyString()
+        {
+            var result = calculate.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestAddForSingleNumber()
+        {
+            var result = calculate.Add("10");
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void TestAddForTwoNumbers()
+        {
+            var result = calculate.Add("15,35");
+            Assert.AreEqual(50, result);
+        }
+
+        [TestMethod]
+        public void TestAddForNewLine()
+        {
+            var result = calculate.Add("\n");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestAddForNewLineWithNumbers()
+        {
+            var result = calculate.Add("1\n2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void TestAddForDelimiters()
+        {
+            var result = calculate.Add("//;\n1;2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestAddForSingleNegativeNumber()
+        {
+            var result = calculate.Add("-10");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestAddForMultipleNegativeNumber()
+        {
+            var result = calculate.Add("-7,-15");
+        }
+    }
+}
